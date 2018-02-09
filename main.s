3d39e9ffd47655009ef41ad61415467e35498ca3
Omar M <distengine@gmail.com>
2018-02-08 19:13:42 -0700

    Help Please

---
+++ main.s
@@ -0,0 +1,115 @@
+;****************** main.s ***************
+; Program written by: Yerraballi
+; Date Created: 1/15/2018 
+; Last Modified: 1/15/2018 
+; Brief description of the program: Solution to Lab1
+; The objective of this system is to implement a Car door signal system
+; Hardware connections: Inputs are negative logic; output is positive logic
+;  PF0 is right-door input sensor (1 means door is open, 0 means door is closed)
+;  PF4 is left-door input sensor (1 means door is open, 0 means door is closed)
+;  PF2 is Safe (Blue) LED signal - ON when both doors are closed, otherwise OFF
+;  PF1 is Unsafe (Red) LED signal - ON when either (or both) doors are open, otherwise OFF
+; The specific operation of this system 
+;   Turn Unsafe LED signal ON if any or both doors are open, otherwise turn the Safe LED signal ON
+;   Only one of the two LEDs must be ON at any time.
+; NOTE: Do not use any conditional branches in your solution. 
+;       We want you to think of the solution in terms of logical and shift operations
+
+GPIO_PORTF_DATA_R  EQU 0x400253FC
+GPIO_PORTF_DIR_R   EQU 0x40025400
+GPIO_PORTF_AFSEL_R EQU 0x40025420
+GPIO_PORTF_PUR_R   EQU 0x40025510
+GPIO_PORTF_DEN_R   EQU 0x4002551C
+GPIO_PORTF_LOCK_R  EQU 0x40025520
+GPIO_PORTF_CR_R    EQU 0x40025524
+GPIO_PORTF_AMSEL_R EQU 0x40025528
+GPIO_PORTF_PCTL_R  EQU 0x4002552C
+GPIO_LOCK_KEY      EQU 0x4C4F434B  ; Unlocks the GPIO_CR register
+SYSCTL_RCGCGPIO_R  EQU 0x400FE608
+       THUMB
+       AREA    DATA, ALIGN=2
+;global variables go here
+      ALIGN
+      AREA    |.text|, CODE, READONLY, ALIGN=2
+      EXPORT  Start
+Start
+	BL  PortF_Init
+  ; initialization code goes here
+loop   
+	LDR r0, =FIFTHSEC
+	BL delay
+	
+	; this gets the input from the FPORT
+    LDR R1, =GPIO_PORTF_DATA_R ; pointer to Port F data
+    LDR R0, [R1]               ; read all of Port F
+    AND R0,R0,#0x11            ; just the input pins PF0 and PF4
+
+	; this sets the LED
+    LDR R1, =GPIO_PORTF_DATA_R ; pointer to Port F data
+    STR R0, [R1]               ; write to PF3-1
+
+	B   loop
+         
+		 
+
+;------------delay------------
+; Delay function for testing, which delays about 3*count cycles.
+; Input: R0  count
+; Output: none
+ONESEC             EQU 5333333      ; approximately 1s delay at ~16 MHz clock
+QUARTERSEC         EQU 1333333      ; approximately 0.25s delay at ~16 MHz clock
+FIFTHSEC           EQU 1066666      ; approximately 0.2s delay at ~16 MHz clock
+delay
+    SUBS R0, R0, #1                 ; R0 = R0 - 1 (count = count - 1)
+    BNE delay                       ; if count (R0) != 0, skip to 'delay'
+    BX  LR                          ; return
+	
+	
+
+;------------PortF_Init------------
+; Initialize GPIO Port F for negative logic switches on PF0 and
+; PF4 as the Launchpad is wired.  Weak internal pull-up
+; resistors are enabled, and the NMI functionality on PF0 is
+; disabled.  Make the RGB LED's pins outputs.
+; Input: none
+; Output: none
+; Modifies: R0, R1, R2
+PortF_Init
+    LDR R1, =SYSCTL_RCGCGPIO_R      ; 1) activate clock for Port F
+    LDR R0, [R1]
+    ORR R0, R0, #0x20               ; set bit 5 to turn on clock
+    STR R0, [R1]
+    NOP
+    NOP                             ; allow time for clock to finish
+    LDR R1, =GPIO_PORTF_LOCK_R      ; 2) unlock the lock register
+    LDR R0, =0x4C4F434B             ; unlock GPIO Port F Commit Register
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_CR_R        ; enable commit for Port F
+    MOV R0, #0xFF                   ; 1 means allow access
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_AMSEL_R     ; 3) disable analog functionality
+    MOV R0, #0                      ; 0 means analog is off
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_PCTL_R      ; 4) configure as GPIO
+    MOV R0, #0x00000000             ; 0 means configure Port F as GPIO
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_DIR_R       ; 5) set direction register
+    MOV R0,#0x0E                    ; PF0 and PF7-4 input, PF3-1 output
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_AFSEL_R     ; 6) regular port function
+    MOV R0, #0                      ; 0 means disable alternate function
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_PUR_R       ; pull-up resistors for PF4,PF0
+    MOV R0, #0x11                   ; enable weak pull-up on PF0 and PF4
+    STR R0, [R1]
+    LDR R1, =GPIO_PORTF_DEN_R       ; 7) enable Port F digital port
+    MOV R0, #0xFF                   ; 1 means enable digital I/O
+    STR R0, [R1]
+    BX  LR
+
+
+    ALIGN                           ; make sure the end of this section is aligned
+    END                             ; end of file
+	
+	
+	
