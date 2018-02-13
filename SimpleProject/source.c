270764eb880daabc386459e4aacaa396b8ebe825
Omar M. Laptop <distengine@gmail.com>
2018-02-12 18:56:29 -0700

    HW2 done, add64 in C is implemented to calc the carry

--- SimpleProject/source.c
+++ SimpleProject/source.c
@@ -1,7 +1,35 @@
-volatile unsigned long m;
+#include <stdint.h>
 
-unsigned long Random(void) 
+typedef struct int64 int64;
+
+unsigned long m;
+
+unsigned long Random() 
 {
 	m = 1664525 * m + 1013904223;
 	return m;
+}
+
+struct int64
+{
+	int32_t lo32;
+	int32_t hi32;
+};
+
+void add_64(int64 *result, int64 *oper1, int64 *oper2)
+{
+	result->lo32 = oper1->lo32 + oper2->lo32;
+	result->hi32 = oper1->hi32 + oper2->hi32;
+	if(result->lo32 < oper1->lo32)
+		++result->hi32;
+}
+
+int main()
+{
+	int64 result, oper1, oper2;
+	oper1.lo32 = Random();
+	oper1.hi32 = Random();
+	oper2.lo32 = Random();
+	oper2.hi32 = Random();
+	add_64(&result, &oper1, &oper2);
 }
